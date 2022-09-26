8fdc20438212da3d4d6bf1f4efbb6b9064a58bb2
victor <victorsleiman25@gmail.com>
2022-09-26 18:01:52 -0300

    create your index file and try to do a connection

---
+++ index.js
@@ -0,0 +1,36 @@
+const {
+Connection,
+PublicKey,
+clusterApiUrl,
+Keypair,
+LAMPORTS_PER_SQL
+} = require("@solana/web3.js")
+
+
+const wallet = new Keypair();
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+//const secretKey = new wallet._keypair.secretKey
+
+
+const getWalletBalance =async()=>{
+
+    try{
+
+       const connection = new Connection(clusterApiUrl('devnet'),'confirmed')
+       const walletBalance = await connection.getBalance(publicKey)
+       console.log(`wallet balance is: ${walletBalance}`)
+
+    }catch(err){
+
+        console.error(err)
+    }
+}
+
+
+const main = async() =>{
+
+    await getWalletBalance()
+}
+
+main()
